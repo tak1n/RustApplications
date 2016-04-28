014116b2b61e4803bc0196e271b82679da4e7f7b
Benny Klotz <benny.klotz92@gmail.com>
2016-04-28 18:15:01 +0200

    4.5 if

---
+++ if.rs
@@ -0,0 +1,21 @@
+fn main() {
+  let x = 5;
+
+  if x == 5 {
+    println!("x is five!");
+  } else if x == 6 {
+    println!("x is six!");
+  } else {
+    println!("x is not five or six :(");
+  }
+
+  let x = 6;
+
+  let y = if x == 5 {
+    10
+  } else {
+    15
+  }; // y: i32
+
+  println!("{}", y);
+}
